86a3cea02c6e9a40fd99a3a32d60ff1623fc0a01
Chris Violette <cvioletterun@gmail.com>
2018-03-09 15:08:53 -0500

    Updated instructions and functionality surrounding clipboard

--- src/components/OptionText.js
+++ src/components/OptionText.js
@@ -15,15 +15,33 @@ class OptionText extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      copied: false
+    }
+
     this.textStyle = {
       fontWeight: 'normal',
       fontSize: '14px',
-      fontFamily: 'Helvetica, Arial, sans-serif',
+      fontFamily: 'Calibri, Helvetica, Arial, sans-serif',
       lineHeight: 1.1,
       marginBottom: 0
     };
 
     const clipboard = new Clipboard('.copy-btn');
+
+    clipboard.on('success', (e) => {
+      this.setState({
+        copied: true
+      });
+
+      e.clearSelection();
+
+      setTimeout(() => {
+        this.setState({
+          copied: false
+        });
+      }, 3000);
+    });
 
     this.handleDownload = this.handleDownload.bind(this);
   }
@@ -42,10 +60,7 @@ class OptionText extends Component {
     return (
       <div>
         <div className="signature--container">
-          <div className="row justify-content-center">
-            <div className="signature--text__image">
-              <img src={Logo} alt="PCHC20 Logo" />
-            </div>
+          <div className="row justify-content-center" id="fullsig">
             <div className="signature--text__text" id="sigtext">
               <p style={{
                   ...this.textStyle,
@@ -93,41 +108,32 @@ class OptionText extends Component {
                   }
                   <a href="https://pchc20.com">pchc20.com</a>
               </p>
+              <p style={{
+                  ...this.textStyle
+                }}>&nbsp;</p>
+              <img src={Logo} alt="PCHC20 Logo" />
             </div>
           </div>
         </div>
-        <div className="row justify-content-center">
-          <div className="card-deck">
-            <div className="card">
-              <div className="card-header">
-                Step 1
-              </div>
-              <div className="card-body">
-                <p>Download a copy of the PCHC20 logo:</p>
-                <p><a href={Logo} download className="btn btn-primary" ref="dl">Download <FontAwesomeIcon icon="download" /></a>
-                </p>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-header">
-                Step 2
-              </div>
-              <div className="card-body">
-                <p>Copy the signature text to paste into your email application:</p>
-                <a className="btn btn-primary copy-btn" ref="copy" data-clipboard-target="#sigtext">Copy <FontAwesomeIcon icon="copy" /></a>
-              </div>
-            </div>
-            <div className="card">
-              <div className="card-header">
-                Step 3
-              </div>
-              <div className="card-body">
-                <p>Add the image and copied text to your signature in your email application.</p>
-              </div>
-            </div>
+        <div className="row">
+          <div className="col">
+            <h3>
+              Step 1
+            </h3>
+            <p>Copy the signature image and text to paste into your email application:</p>
+            <a className="btn btn-primary copy-btn mr-2" ref="copy" data-clipboard-target="#sigtext">Copy <FontAwesomeIcon icon="copy" /></a>
+            { this.state.copied
+              ? <span className="badge badge-success">Copied!</span>
+              : null }
+          </div>
+          <div className="col">
+            <h3>
+              Step 2
+            </h3>
+            <p>Paste the copied text and image into your signature in your email application.</p>
           </div>
         </div>
-        <p className="text-center">
+        <p className="text-center mt-3 mb-3">
           <a
             href="https://support.office.com/en-us/article/create-and-add-a-signature-to-messages-8ee5d4f4-68fd-464a-a1c1-0e1c80bb27f2#ID0EAACAAA=2016,_2013"
             className="btn btn-outline-secondary btn-sm"
